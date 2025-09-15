f29fb845d47b1e549acf3cad0e98e261cdb799c5
gowthamasamy <84223812+gowthamasamy@users.noreply.github.com>
2025-09-15 16:27:12 +0530

    Add files via upload

---
+++ 2.3 sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('family-tree-cache').then(cache =>
+      cache.addAll(['index.html','manifest.json','icon-192.png','icon-512.png'])
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
